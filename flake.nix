d612ead062dae8ac40b4920a964c78ed17cca452
Brandon Li <48413902+aspiringLich@users.noreply.github.com>
2025-03-19 23:42:48 -0400

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            (python3.withPackages (
+              python-pkgs: with python-pkgs; [
+                pyyaml
+                ipykernel
+                requests
+                beautifulsoup4
+              ]
+            ))
+          ];
+        };
+      }
+    );
+}
